84d1e47eb66b58523fd5d58df33e2e287b25dac2
JW Dixon <johnathan@modestarcade.com>
2021-04-21 23:02:25 -0500

    Misc cleanup

--- static/js/site.js
+++ static/js/site.js
@@ -15,3 +15,39 @@ function appendSelectOption(elem, text, value) {
   elem.appendChild(option);
   elem.disabled = false;
 }
+
+function renderAssetSelect(elem, data) {
+  if(data !== undefined) {
+    elem.length = 0;
+
+    if(data.length > 0) {
+    // append assets
+    for (var i = data.length - 1; i >= 0; i--) {
+        appendSelectOption(elem,
+                            data[i]['asset']['params']['name'],
+                            data[i]['asset']['index']);
+      }
+    } else {
+      appendSelectOption(elem,
+                            "No assets found.",
+                            -1);
+    }
+  }
+}
+
+function renderAccountSelect(elem, data) {
+  elem.length = 0;
+
+  if(data.length > 0) {
+    // append accounts
+    for (var i = data.length - 1; i >= 0; i--) {
+      appendSelectOption(elem,
+                          data[i].address,
+                          data[i].address);
+    }
+  } else {
+    appendSelectOption(elem,
+                          "No accounts found.",
+                          -1);
+  }
+}
