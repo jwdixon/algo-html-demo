bd38106d4b261cc4cf90d228bf45cd0a510d3c04
JW Dixon <johnathan@modestarcade.com>
2021-04-24 18:44:43 -0500

    Cleanup, remove "algowrapper.js"

--- static/js/site.js
+++ static/js/site.js
@@ -1,3 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+});
+
+var closeButtonElements = document.getElementsByClassName('delete');
+
+for (var i = 0; i < closeButtonElements.length; i++) {
+  closeButtonElements[i].addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.classList.add('is-hidden');
+  });
+}
+
+
 function renderLoadingSelect(elem) {
   elem.disabled = true;
   elem.length = 0;
